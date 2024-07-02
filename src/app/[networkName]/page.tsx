e8187119bf6851ce78eea5a88b90a12bb98f810a
Atanda1 <atandadave@gmail.com>
2024-07-02 03:26:19 +0100

    fix: metadata

--- src/app/[networkName]/page.tsx
+++ src/app/[networkName]/page.tsx
@@ -8,19 +8,19 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata({ params, searchParams }: Props) {
-  // read route params
-  const name = params.networkName;
-  return {
-    title: `${name.charAt(0).toUpperCase() + name.slice(1)} Protocol Registry`,
-    description: `Explore ${name} protocols and their documentation`,
-    openGraph: {
-      images: [
-        "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
-      ],
-    },
-  };
-}
+// export async function generateMetadata({ params, searchParams }: Props) {
+//   // read route params
+//   const name = params.networkName;
+//   return {
+//     title: `${name.charAt(0).toUpperCase() + name.slice(1)} Protocol Registry`,
+//     description: `Explore ${name} protocols and their documentation`,
+//     openGraph: {
+//       images: [
+//         "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
+//       ],
+//     },
+//   };
+// }
 
 export default function Home() {
   return (
