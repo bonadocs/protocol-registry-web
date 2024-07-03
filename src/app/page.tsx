a75d1fe95f63bf2443e20b3dd41e25ffa4d496dc
Atanda1 <atandadave@gmail.com>
2024-07-03 07:55:49 +0100

    fix metadata problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,38 +3,38 @@ import { Metadata, ResolvingMetadata } from "next";
 
 import "../styles/main.scss";
 
-// export const metadata: Metadata = {
-//   title: "Bonadocs Protocol Registry",
-//   description: "Explore protocols and their documentation",
-//   openGraph: {
-//     images: [
-//       "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
-//     ],
-//   },
-// };
-
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | undefined };
+export const metadata: Metadata = {
+  title: "Bonadocs Protocol Registry",
+  description: "Explore protocols and their documentation",
+  openGraph: {
+    images: [
+      "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
+    ],
+  },
 };
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
+// type Props = {
+//   params: { id: string };
+//   searchParams: { [key: string]: string | undefined };
+// };
 
-  const name = searchParams.chain ?? "Bonadocs";
-  return {
-    title: `${name.charAt(0).toUpperCase() + name.slice(1)} Protocol Registry`,
-    description: `Explore ${name} protocols and their documentation`,
-    openGraph: {
-      images: [
-        "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
-      ],
-    },
-  };
-}
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   // read route params
+
+//   const name = searchParams.chain ?? "Bonadocs";
+//   return {
+//     title: `${name.charAt(0).toUpperCase() + name.slice(1)} Protocol Registry`,
+//     description: `Explore ${name} protocols and their documentation`,
+//     openGraph: {
+//       images: [
+//         "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
+//       ],
+//     },
+//   };
+// }
 
 export default function Home() {
   return <HomeWrapper />;
