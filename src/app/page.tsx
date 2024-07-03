af52aa9666e056d4cb52264f1454a6a0d1eebe07
Atanda1 <atandadave@gmail.com>
2024-07-03 07:01:12 +0100

    fix metadata problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,15 @@ import { Registry } from "@/layout/registry/Registry";
 import type { Metadata } from "next";
 import "../styles/main.scss";
 
-type generateMetadataProps = {
-  searchParams: URLSearchParams;
+export const metadata: Metadata = {
+  title: "Bonadocs Protocol Registry",
+  description: "Explore protocols and their documentation",
+  openGraph: {
+    images: [
+      "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
+    ],
+  },
 };
-
-export async function generateMetadata({
-  searchParams,
-}: generateMetadataProps) {
-  // read route params
-  const name = searchParams.get("chain") ?? "Bonadocs";
-  return {
-    title: `${name.charAt(0).toUpperCase() + name.slice(1)} Protocol Registry`,
-    description: `Explore ${name} protocols and their documentation`,
-    openGraph: {
-      images: [
-        "https://res.cloudinary.com/dfkuxnesz/image/upload/v1719732142/Screenshot_2024-06-30_at_08.21.55_kyfylz.png",
-      ],
-    },
-  };
-}
 
 export default function Home() {
   return (
